963390c74d549d126783b82827db85f229f81e52
Andy <458666482@qq.com>
2018-07-02 23:35:03 +0800

    轮播图

--- app/js/router/index.js
+++ app/js/router/index.js
@@ -1,8 +1,9 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
+import '../../css/reset.scss';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
     routes: [
