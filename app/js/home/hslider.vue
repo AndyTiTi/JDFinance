f900add93a61f833b5db9b40c258b4dfbfb3c39f
andy <458666482@qq.com>
2018-07-03 11:18:41 +0800

    解决eslint报错

--- app/js/home/hslider.vue
+++ app/js/home/hslider.vue
@@ -24,30 +24,30 @@
                     href:'home',
                     src:'//img12.360buyimg.com/jrpmobile/jfs/t22900/231/576526218/45107/39fa9788/5b360917N2b51c415.png?width=750&height=320'
                 },{
-                    href:'home',
+                    href:'home1',
                     src:'//img12.360buyimg.com/jrpmobile/jfs/t21160/246/1781732558/36338/14ddc2ea/5b36090fN459e431e.jpg?width=750&height=320'
                 },{
-                    href:'home',
+                    href:'home2',
                     src:'//img12.360buyimg.com/jrpmobile/jfs/t22987/219/562677820/50700/8c800389/5b360912Nfc5e66c5.jpg?width=750&height=320'
                 }],
                 enters:[{
-                    href:'home',
-                    img:'//img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132',
+                    href:'home3',
+                    img:'//img12.360buyimg.com/jrpmobile/jfs/t14896/125/1435676873/3668/a8b392f7/5a4f26edN6cdfda66.png?width=88&height=88',
                     title:'优惠券'
                 },{
-                    href:'home',
+                    href:'home4',
                     img:'//img12.360buyimg.com/jrpmobile/jfs/t5590/252/875247023/17343/946aa72c/59224650N0f7ffc92.png?width=132&height=132',
                     title:'领红包'
                 },{
-                    href:'home',
+                    href:'home5',
                     img:'//img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132',
                     title:'抢钢镚'
                 },{
-                    href:'home',
+                    href:'home6',
                     img:'//img12.360buyimg.com/jrpmobile/jfs/t4393/329/2180608902/13217/c88c0cec/58ec9dcdN1534e2d7.png?width=132&height=132',
                     title:'抢京豆'
                 },{
-                    href:'home',
+                    href:'home7',
                     img:'//img12.360buyimg.com/jrpmobile/jfs/t5488/298/1036263348/12073/b4f4de97/590ac8e8Ne9def22e.png?width=135&height=135',
                     title:'白条提额'
                 }]
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -1,16 +1,14 @@
 import Vue from "vue"
 import Router from "vue-router"
-import Home from "../home/index.vue"
-import '../../css/reset.scss';
-
-Vue.use(Router);
-
+import home from "../home/index.vue"
+import "../../css/reset.scss"
+Vue.use(Router)
 export default new Router({
     routes: [
         {
             path: "/",
-            name: "Home",
-            component: Home,
+            name: "home",
+            component: home,
         },
     ],
 })
